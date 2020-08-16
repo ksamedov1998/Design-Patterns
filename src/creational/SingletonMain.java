9f8120e96efdfd1613743701a889b3b87d37aee0
ksamedov1998 <ksamedov1998@gmail.com>
2020-08-17 01:02:50 +0400

    Lazy init singleton and ThreadSafeSingleton created

--- src/creational/SingletonMain.java
+++ src/creational/SingletonMain.java
@@ -1,7 +1,6 @@
 package creational;
 
 /**
- * @implNote
  * Real time examples that uses creational.Singleton design pattern:
  *      - Runtime
  *      - Logger
@@ -11,6 +10,12 @@ package creational;
  *  Class is responsible for lifecycle
  *  ** Needs to be thread safe
  *  private instance and should have private constructor
+ * */
+
+/**
+ * @implNote
+ * eager loaded singleton creates object when application start to run
+ * simply when even you don't need it, it will be there (in memory).
  * */
 class SimpleSingleton {
     private static SimpleSingleton singleton = new SimpleSingleton();
@@ -22,25 +27,72 @@ class SimpleSingleton {
     }
 }
 
+/**
+ * @implNote  When you use Lazy loading option u gain some advantages of to run application
+ * start time will be less and does not create an instance until it is needed.
+ *
+ * */
+class LazyLoadedSingleton {
+    private static LazyLoadedSingleton singleton = null;
+    private LazyLoadedSingleton(){}
+
+    public static LazyLoadedSingleton getInstance(){
+            if(singleton == null){
+                singleton = new LazyLoadedSingleton();
+            }
+        return singleton;
+    }
+}
+
+// todo can be check with multiple thread
+class ThreadSafeSingleton {
+    private static volatile ThreadSafeSingleton singleton = null;
+    private ThreadSafeSingleton(){
+            // can not be instantiated by reflection or JVM stuff
+        if(singleton != null) {
+            throw new RuntimeException("Use getInstance method to create!");
+        }
+    }
+
+    public static ThreadSafeSingleton getInstance(){
+
+        if(singleton == null){
+            synchronized (ThreadSafeSingleton.class){
+                if(singleton == null){
+                    singleton = new ThreadSafeSingleton();
+                }
+            }
+        }
+        return singleton;
+    }
+}
+
 public class SingletonMain{
     public static void main(String[] args) {
         processSimpleSingleton();
+        processLazyLoadedSingleton();
+    }
+
+    private static void processLazyLoadedSingleton() {
+        LazyLoadedSingleton simpleSingleton = LazyLoadedSingleton.getInstance();
+        LazyLoadedSingleton simpleSingleton1 = LazyLoadedSingleton.getInstance();
+        checkEquality(simpleSingleton,simpleSingleton1,"Simple singleton");
     }
 
     private static void processSimpleSingleton() {
         SimpleSingleton simpleSingleton = SimpleSingleton.getInstance();
         SimpleSingleton simpleSingleton1 = SimpleSingleton.getInstance();
-        checkEquality(simpleSingleton,simpleSingleton1);
+        checkEquality(simpleSingleton,simpleSingleton1,"Lazy loaded singleton");
     }
 
-    private static void checkEquality(SimpleSingleton s1,SimpleSingleton s2){
+    private static <U> void checkEquality(U s1,U s2,final String type){
         if(s1== s2){
-            printMessage("Simple Singleton",true);
+            printMessage(type ,true);
         }else{
-            printMessage("Simple Singleton",false);
+            printMessage(type ,false);
         }
     }
     private static void printMessage(String name,boolean isCorrect) {
-        System.out.printf("Both object are the same,%s implemented %s ",name,isCorrect?"successfully":"deficient");
+        System.out.printf("Both object are the same,%s implemented %s \n",name,isCorrect?"successfully":"deficient");
     }
 }
